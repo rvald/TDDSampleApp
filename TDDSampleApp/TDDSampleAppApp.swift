0cb1b7814470f1580559e741db5658aaaffc5dcc
rvald <rsvald84@gmail.com>
2024-01-25 14:48:45 -0500

    e - new xcode project with interface: swiftui, language: swift, unit tests: on

---
+++ TDDSampleApp/TDDSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDSampleAppApp.swift
+//  TDDSampleApp
+//
+//  Created by Rene Valdes on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TDDSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
