c6544bb6494086c993f27b3045fb8760018f83f5
rvald <rsvald84@gmail.com>
2024-01-25 16:21:38 -0500

    t - remove apple placeholder tests

---
+++ TDDSampleAppTests/PlaceholderTests.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceholderTests.swift
+//  TDDSampleAppTests
+//
+//  Created by Rene Valdes on 1/25/24.
+//
+
+@testable import TDDSampleApp
+import XCTest
+
+@MainActor
+final class PlaceholderTests: XCTestCase {
+    func test_zero() throws {
+    }
+}
