f224e03431e7f9f0bfde825d10d8c6b6afec40ae
rvald <rsvald84@gmail.com>
2024-01-25 15:34:22 -0500

    Rgit status change app entry point to MainEntryPoint

---
+++ TDDSampleApp/Main.swift
@@ -0,0 +1,15 @@
+//
+//  Main.swift
+//  TDDSampleApp
+//
+//  Created by Rene Valdes on 1/25/24.
+//
+
+import Foundation
+
+@main
+struct MainEntryPoint {
+    static func main() {
+        TDDSampleAppApp.main()
+    }
+}
--- TDDSampleApp/TDDSampleAppApp.swift
+++ TDDSampleApp/TDDSampleAppApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-@main
+
 struct TDDSampleAppApp: App {
     var body: some Scene {
         WindowGroup {
