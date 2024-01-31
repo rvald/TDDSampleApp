56156ea71faed9208516349895a83bc752ef5934
rvald <rsvald84@gmail.com>
2024-01-31 02:23:32 -0500

    R - refactor Topic initializatito setUp and tearDown method

--- TDDSampleAppTests/TopicTests.swift
+++ TDDSampleAppTests/TopicTests.swift
@@ -11,18 +11,25 @@ import XCTest
 @MainActor
 final class TopicTests: XCTestCase {
     
+    var topic: Topic!
+    
+    override func setUp()  {
+        topic = Topic(name: "iPhone", tag: "iPhone")
+    }
+    
+    override func tearDown() {
+        topic = nil
+    }
+    
     func testThatTopicExists() throws {
-        let topic = Topic(name: "", tag: "")
         XCTAssertNotNil(topic)
     }
     
     func testThatTopicCanBeNamed() throws {
-        let namedTopic = Topic(name: "iPhone", tag: "")
-        XCTAssertEqual(namedTopic.name, "iPhone", "the topic should have the name I gave it")
+        XCTAssertEqual(topic.name, "iPhone", "the topic should have the name I gave it")
     }
     
     func testThatTopicHasTag() throws {
-        let taggedTopic = Topic(name: "", tag: "iPhone")
-        XCTAssertEqual(taggedTopic.tag, "iPhone", "Topics need to have tags")
+        XCTAssertEqual(topic.tag, "iPhone", "Topics need to have tags")
     }
 }
