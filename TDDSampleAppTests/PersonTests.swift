365bdaa9b5bf057432f8bd8b2801b2f9e35f5006
rvald <rsvald84@gmail.com>
2024-01-31 03:13:54 -0500

    t - add PersonTests with test_zero test

---
+++ TDDSampleAppTests/PersonTests.swift
@@ -0,0 +1,15 @@
+//
+//  PersonTests.swift
+//  TDDSampleAppTests
+//
+//  Created by Rene Valdes on 1/31/24.
+//
+
+import XCTest
+@testable import TDDSampleApp
+
+final class PersonTests: XCTestCase {
+    func test_zero() throws {
+        
+    }
+}
