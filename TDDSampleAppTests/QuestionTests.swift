8e0137dafe70c1a939601c327ffe5aaaad61771a
rvald <rsvald84@gmail.com>
2024-01-31 02:41:05 -0500

    t - add QuestionTests with empty test_zero

---
+++ TDDSampleAppTests/QuestionTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionTests.swift
+//  TDDSampleAppTests
+//
+//  Created by Rene Valdes on 1/31/24.
+//
+
+import XCTest
+@testable import TDDSampleApp
+
+@MainActor
+final class QuestionTests: XCTestCase {
+    func testZero() throws {
+        
+    }
+}
