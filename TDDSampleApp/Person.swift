31ee4fb4481ec2648386a1d0ec9744a715e9479b
rvald <rsvald84@gmail.com>
2024-01-31 03:21:47 -0500

    t - add testThatPersonHasTheRightName and testThatPersonHasAnAvatarURL tests, with setUp and tearDown refactor

---
+++ TDDSampleApp/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  TDDSampleApp
+//
+//  Created by Rene Valdes on 1/31/24.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let avatarLocation: String
+}
--- TDDSampleAppTests/PersonTests.swift
+++ TDDSampleAppTests/PersonTests.swift
@@ -9,7 +9,23 @@ import XCTest
 @testable import TDDSampleApp
 
 final class PersonTests: XCTestCase {
-    func test_zero() throws {
-        
+    
+    var person: Person!
+    
+    override func setUp() {
+        person = Person(name: "Bruce Wayne", avatarLocation: "https://example.com/avatar.jpg")
+    }
+    
+    override func tearDown() {
+        person = nil
+    }
+    
+    func testThatPersonHasTheRightName() throws {
+        XCTAssertEqual(person.name, "Bruce Wayne", "expecting a person to proive its name")
+    }
+    
+    func testThatPersonHasAnAvatarURL() {
+        let url = URL(string: "https://example.com/avatar.jpg")!
+        XCTAssertEqual(person.avatarLocation, url.absoluteString, "The person's avatar should be represented by a url")
     }
 }
